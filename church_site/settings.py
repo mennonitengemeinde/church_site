2866264cb3e4201a8ba6d41d83f9d60036c32a54
Daniel Braun <daniel@pomas.ca>
2020-05-14 17:05:59 -0400

    changed vendors for docker

--- church_site/settings.py
+++ church_site/settings.py
@@ -173,8 +173,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
-    os.path.join(BASE_DIR, 'node_modules')
+    os.path.join(BASE_DIR, 'assets')
 ]
 
 STATICFILES_FINDERS = (
