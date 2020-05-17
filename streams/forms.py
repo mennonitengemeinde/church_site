1e2bf7358175544177247fe70981f45d7e7bec29
Daniel Braun <daniel@pomas.ca>
2020-05-17 11:55:12 -0400

    fixes #19

--- streams/forms.py
+++ streams/forms.py
@@ -7,7 +7,7 @@ from streams.models import Stream
 class StreamCreateForm(ModelForm):
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['event'].queryset = Event.objects.member_only_events(user=user)
+        self.fields['event'].queryset = Event.objects.member_only_events(user=user).order_by('-start')
 
     class Meta:
         model = Stream
