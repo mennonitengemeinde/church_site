57de5966cd014c216bf77f72712bbc28bf4ff23d
Daniel Braun <daniel@pomas.ca>
2020-06-16 18:22:35 -0400

    changed person model a bit

--- memberships/models.py
+++ memberships/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django_countries.fields import CountryField
 
 from churches.models import Church
 
@@ -18,7 +19,6 @@ class Membership(models.Model):
     reason_for_leaving = models.TextField(null=True, blank=True)
     verse = models.CharField(max_length=200, null=True, blank=True)
     active = models.BooleanField(default=True)
-    pastor = models.CharField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return f'{self.church} - {self.person} - {self.start_date}'
@@ -26,6 +26,8 @@ class Membership(models.Model):
 
 class Family(models.Model):
     name = models.CharField(max_length=200, unique=True)
+    father = models.ForeignKey('Person', on_delete=models.PROTECT, related_name='father_of')
+    mother = models.ForeignKey('Person', on_delete=models.PROTECT, related_name='mother_of')
 
     def __str__(self):
         return self.name
@@ -41,10 +43,16 @@ class Person(models.Model):
     date_birth = models.DateField(null=True, blank=True)
     contact_number = models.CharField(max_length=20, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
+    street = models.CharField(max_length=250)
+    city = models.CharField(max_length=50)
+    province_state = models.CharField(max_length=50)
+    country = CountryField()
     marital_status = models.IntegerField(max_length=50, choices=marital_status)
+    passed_away = models.ForeignKey('Family', on_delete=models.PROTECT, related_name='passed_away',
+                                    null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    member_of = models.ForeignKey(Church, on_delete=models.PROTECT, related_name='church_members')
     speaker = models.BooleanField(default=False)
-    father_of = models.OneToOneField(Family, on_delete=models.PROTECT, related_name='father', blank=True, null=True)
-    mother_of = models.OneToOneField(Family, on_delete=models.PROTECT, related_name='mother', blank=True, null=True)
     spouse = models.OneToOneField('Person', on_delete=models.PROTECT, related_name='spouse', blank=True, null=True)
     child_of = models.ForeignKey(Family, on_delete=models.PROTECT, related_name='children', blank=True, null=True)
 
