c13071366046b956fd9a012d249e4b6e8cfb4963
Daniel Braun <daniel@pomas.ca>
2020-06-30 21:58:30 -0400

    added auth group management

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from accounts.views import UsersAdminListView, UsersAdminUpdateView
+from accounts.views import (
+    GroupsAdminCreateView,
+    GroupsAdminListView,
+    GroupsAdminUpdateView,
+    UsersAdminListView,
+    UsersAdminUpdateView
+)
 
 app_name = 'accounts'
 
@@ -8,4 +14,7 @@ urlpatterns = [
     # path('login/', LoginView.as_view(), name='login'),
     path('manage/users/', UsersAdminListView.as_view(), name='users-admin-list'),
     path('manage/users/<int:pk>/update', UsersAdminUpdateView.as_view(), name='users-admin-update'),
+    path('manage/groups/', GroupsAdminListView.as_view(), name='groups-admin-list'),
+    path('manage/groups/add', GroupsAdminCreateView.as_view(), name='groups-admin-create'),
+    path('manage/groups/<int:pk>/update', GroupsAdminUpdateView.as_view(), name='groups-admin-update'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import Group
 from django.db.models import Q
 from django.urls import reverse_lazy
 
 from accounts.forms import UpdateUserForm
 from accounts.models import User
-from church_site.views import AdminListView, BaseUpdateView
+from church_site.views import AdminListView, BaseUpdateView, BaseCreateView
 
 
 class UsersAdminListView(PermissionRequiredMixin, AdminListView):
@@ -39,3 +40,34 @@ class UsersAdminUpdateView(PermissionRequiredMixin, BaseUpdateView):
         else:
             return User.objects.filter(Q(member__in=self.request.user.churches.all()) | Q(member__isnull=True)).distinct()
 
+
+class GroupsAdminListView(PermissionRequiredMixin, AdminListView):
+    permission_required = 'auth.view_group'
+    model = Group
+    context_object_name = 'groups'
+    template_name = 'accounts/groups-admin-list.html'
+    page_title = 'Groups - Admin'
+    current_page = 'manage'
+    btn_add_href = reverse_lazy('accounts:groups-admin-create')
+
+
+class GroupsAdminCreateView(PermissionRequiredMixin, BaseCreateView):
+    permission_required = 'auth.add_group'
+    model = Group
+    fields = ['name', 'permissions']
+    template_name = 'admin-form-view.html'
+    success_url = reverse_lazy('accounts:groups-admin-list')
+    page_title = 'New Group - Admin'
+    current_page = 'manage'
+    btn_back_href = reverse_lazy('accounts:groups-admin-list')
+
+
+class GroupsAdminUpdateView(PermissionRequiredMixin, BaseUpdateView):
+    permission_required = 'auth.change_group'
+    model = Group
+    fields = ['name', 'permissions']
+    template_name = 'admin-form-view.html'
+    success_url = reverse_lazy('accounts:groups-admin-list')
+    page_title = 'Update Group - Admin'
+    current_page = 'manage'
+    btn_back_href = reverse_lazy('accounts:groups-admin-list')
