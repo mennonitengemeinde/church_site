cc5e15c1d383802e0de831588bf84f1b973b26f9
Daniel Braun <daniel@pomas.ca>
2021-01-09 11:02:51 -0500

    fixed dotenv

--- church_site/settings.py
+++ church_site/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Load dotenv
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
--- manage.py
+++ manage.py
@@ -3,11 +3,8 @@
 import os
 import sys
 
-from dotenv import load_dotenv
-
 
 def main():
-    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'church_site.settings')
     try:
         from django.core.management import execute_from_command_line
