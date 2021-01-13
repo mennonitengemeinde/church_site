955dfd1f68e32402f754ad6c520650c3668d3fca
Daniel Braun <42745795+danielpomas@users.noreply.github.com>
2021-01-12 23:36:48 -0500

    Fix env variable

--- church_site/settings.py
+++ church_site/settings.py
@@ -107,7 +107,7 @@ if env('PRODUCTION_DB', default=True):
             'USER': env('DB_USER'),
             'PASSWORD': env('DB_PASS'),
             'HOST': env('DB_HOST', default='localhost'),
-            'PORT': 5432
+            'PORT': env('DB_PORT', default=5432)
         }
     }
 
