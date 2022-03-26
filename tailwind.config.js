cc431e60f345496999fe754238ecf6b2d617ab41
Daniel Braun <daniel@pomas.ca>
2022-03-26 18:49:41 -0400

    Added assets for home screem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@ module.exports = {
   content: ["./templates/**/*.{html,js}", "./home/templates/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "background-pattern": "url('/static/img/background.svg')",
+        "hero-background": "url('/static/img/hero-texture-33-4.jpg')",
+      },
       colors: {
-        primary: '#602627'
-
-      }
+        primary: "#602627"
+      },
     },
   },
   plugins: [],
