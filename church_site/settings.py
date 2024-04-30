ab5bf3f5f337511567bdca38aee04b67fd2f3b07
Daniel Braun <42745795+danielpomas@users.noreply.github.com>
2024-04-29 21:26:37 -0400

    changed google provider setting

--- church_site/settings.py
+++ church_site/settings.py
@@ -187,18 +187,20 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SOCIALACCOUNT_PROVIDERS = {
-    #     "APP": {
-    #         "client_id": env("GOOGLE_CLIENT_ID"),
-    #         "secret": env("GOOGLE_CLIENT_SECRET"),
-    #     },
-    #     "SCOPE": [
-    #         "profile",
-    #         "email",
-    #     ],
-    #     "AUTH_PARAMS": {
-    #         "access_type": "online",
-    #     },
-    # }
+    # "APP": {
+    #     "client_id": env("GOOGLE_CLIENT_ID"),
+    #     "secret": env("GOOGLE_CLIENT_SECRET"),
+    # },
+
+    "google": {
+        "SCOPE": [
+            "profile",
+            "email",
+        ],
+        "AUTH_PARAMS": {
+            "access_type": "online",
+        },
+    }
 }
 
 # Rest
